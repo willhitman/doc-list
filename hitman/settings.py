d5003d2812640ad4072f700eb4dc0779a73a056e
willhitman <giftwt9wt@gmail.com>
2024-08-16 13:06:17 +0200

    adding mysql db hosted locally

--- hitman/settings.py
+++ hitman/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'hitman.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'doc_list',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
