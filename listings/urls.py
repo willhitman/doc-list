d15fd541a66ca7868cbb7c623060eb2daf76a09d
willhitman <giftwt9wt@gmail.com>
2024-08-16 08:53:19 +0200

    adding new end points

--- listings/urls.py
+++ listings/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.CreateListingView.as_view(), name='create-lisitng'),
+    path('<int:pk>/', views.GetUpdateDeleteListingView.as_view(), name='get-update-destroy'),
+    path('get-all-listings/', views.GetAllListingsView.as_view(), name='get-all-listings'),
+    path('get-by-user-id/<int:user_id>/', views.GetAllListingsByUserIdView.as_view(), name='get-by-user-id'),
+    path('get-by-listing-type/<str:listing_type>/', views.GetListingsByListingTypeView.as_view(), name='get-all-by-listing-type')
 ]
+
