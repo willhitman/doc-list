e67dd0d371ea26c06b54fbc22b114382e6f690ae
willhitman <giftwt9wt@gmail.com>
2024-08-27 09:20:11 +0200

    adding views and urls for services

---
+++ utils/service/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('service/', views.CreateServiceView.as_view(), name='service'),
+    path('service/<int:pk>/', views.GetUpdateDestroyServicesView.as_view(),
+         name='service-get-update-destroy'),
+    path('get-all-service/', views.GetAllServicesView.as_view(),
+         name='get-all-service')
+]
---
+++ utils/service/views.py
@@ -0,0 +1,54 @@
+from rest_framework import status
+from rest_framework.generics import CreateAPIView, GenericAPIView, ListAPIView
+from rest_framework.response import Response
+
+from utils.models import Services
+from utils.serializers import ServicesSerializer
+
+
+class CreateServiceView(CreateAPIView):
+    serializer_class = ServicesSerializer
+    queryset = Services.objects.all()
+    authentication_classes = []
+
+
+class GetUpdateDestroyServicesView(GenericAPIView):
+    serializer_class = ServicesSerializer
+    queryset = Services.objects.all()
+    authentication_classes = []
+
+    def get(self, request, pk):
+        try:
+            service = self.queryset.get(pk=pk)
+        except Services.DoesNotExist:
+            return Response({'message': 'Service not found'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            serializer = self.serializer_class(service)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        try:
+            service = self.queryset.get(pk=pk)
+        except Services.DoesNotExist:
+            return Response({'message': 'Service not found'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            serializer = self.serializer_class(service, request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        try:
+            service = self.queryset.get(pk=pk)
+        except Services.DoesNotExist:
+            return Response({'message': 'Service not found'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            service.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class GetAllServicesView(ListAPIView):
+    serializer_class = ServicesSerializer
+    queryset = Services.objects.all()
+    authentication_classes = []
