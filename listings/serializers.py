a64fe56dd5935aef0ac2455fe0c198793bccd46b
willhitman <giftwt9wt@gmail.com>
2024-08-28 08:27:46 +0200

    ...

--- listings/serializers.py
+++ listings/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.utils import timezone
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
@@ -9,27 +10,53 @@ from utils.models import Address, Languages, Services
 from utils.serializers import AddressSerializer, LanguagesSerializer, ServicesSerializer
 
 
+# function to handle creation or update of listing languages
+def handle_languages(listing, languages_data):
+    existing_languages = set(listing.languages.all())
+    new_languages = []
+
+    for lang in languages_data:
+        language_instance, created = ListingLanguages.objects.get_or_create(**lang)
+        if language_instance not in existing_languages:
+            new_languages.append(language_instance)
+
+    if new_languages:
+        listing.languages.add(*new_languages)
+        listing.save()
+
+    ListingLanguages.objects.filter(listings=None).delete()
+
+
 class ListingLanguageSerializer(ModelSerializer):
     class Meta:
         model = ListingLanguages
         exclude = ['date_created', 'last_updated']
 
 
-class ListingLanguageCreateSerializer(serializers.Serializer):
+class ListingLanguageCreateUpdateSerializer(serializers.Serializer):
     listing_id = serializers.IntegerField(min_value=1, required=True)
     languages = ListingLanguageSerializer(many=True, required=True)
 
     def create(self, validated_data):
         listing_id = validated_data.pop('listing_id')
-        languages = validated_data.pop('languages', [])
+        languages_data = validated_data.pop('languages', [])
 
-        listing = Listing.objects.get(pk=listing_id)
+        try:
+            listing = Listing.objects.get(pk=listing_id)
+        except Listing.DoesNotExist:
+            raise serializers.ValidationError({"listing_id": "Listing not found."})
 
-        _languages = [ListingLanguages.objects.create(**lang) for lang in languages]
-
-        listing.languages.set(_languages)
-        listing.save()
+        with transaction.atomic():
+            handle_languages(listing=listing, languages_data=languages_data)
         return listing
+
+    def update(self, instance, validated_data):
+        languages_data = validated_data.pop('languages', [])
+
+        with transaction.atomic():
+            handle_languages(listing=instance, languages_data=languages_data)
+
+        return instance
 
 
 class ListingSerializer(ModelSerializer):
@@ -41,37 +68,38 @@ class ListingSerializer(ModelSerializer):
         exclude = ['date_created', 'last_updated']
 
     def create(self, validated_data):
-        address_data = validated_data.pop("address")
+        address_data = validated_data.pop("address", None)
         languages = validated_data.pop("languages", [])
-
-        if address_data:
-            address = Address.objects.create(**address_data)
-            validated_data['address'] = address
-        listing = Listing.objects.create(**validated_data)
-
-        _languages = [ListingLanguages.objects.create(**lang) for lang in languages]
-        listing.languages.set(_languages)
+        with transaction.atomic():
+            if address_data:
+                address = Address.objects.create(**address_data)
+                validated_data['address'] = address
+            listing = Listing.objects.create(**validated_data)
+            if languages:
+                handle_languages(listing=listing, languages_data=languages)
 
         return listing
 
     def update(self, instance, validated_data):
         address_data = validated_data.pop("address", None)
         languages_data = validated_data.pop("languages", [])
+        with transaction.atomic():
+            if address_data:
+                address, created = Address.objects.update_or_create(
+                    id=instance.address.pk,
+                    defaults=address_data
+                )
+                instance.address = address
 
-        if address_data:
-            address, created = Address.objects.update_or_create(
-                id=instance.address.pk,
-                defaults=address_data
-            )
-            instance.address = address
+                for attr, value in validated_data.items():
+                    setattr(instance, attr, value)
 
-            for attr, value in validated_data.items():
-                setattr(instance, attr, value)
+                instance.save()
 
-            instance.save()
+                if languages_data:
+                    handle_languages(listing=instance, languages_data=languages_data)
 
-            if languages_data is not None:
-                instance.languages.set(languages_data)
+                ListingLanguages.objects.filter(listings=None).delete()
 
         return instance
 
@@ -183,7 +211,6 @@ class AppointmentsAvailabilitySerializer(ModelSerializer):
 
 
 class ListingServicesSerializer(ModelSerializer):
-    service = ServicesSerializer(many=True)
 
     class Meta:
         model = ListingServices
@@ -191,17 +218,36 @@ class ListingServicesSerializer(ModelSerializer):
 
     def create(self, validated_data):
         service_data = validated_data.pop('service', [])
-        _services = [Services.objects.create(**serv) for serv in service_data]
+        with transaction.atomic():
+            _services = [Services.objects.create(**serv) for serv in service_data]
 
-        listing_service = ListingServices.objects.create(**validated_data)
-        listing_service.service.set(_services)
-        listing_service.save()
-        return listing_service
+            listing_service = ListingServices.objects.create(**validated_data)
+            listing_service.service.set(_services)
+            listing_service.save()
+            return listing_service
 
+    def update(self, instance, validated_data):
+        service_data = validated_data.pop('service', [])
+        existing_services = set(instance.service.all())
+        with transaction.atomic():
+            for attr, value in validated_data.items():
+                setattr(instance, attr, value)
+            instance.save()
+
+            new_services = []
+
+            for service in service_data:
+                service_instance, created = ListingServices.objects.get_or_create(**service)
+                if service_instance not in existing_services:
+                    new_services.append(service_instance)
+
+            if new_services:
+                instance.service.add(*new_services)
+
+            ListingServices.objects.filter(listing=None).delete()
 
 
 class ListingServicesUpdateSerializer(ModelSerializer):
-
     class Meta:
         model = ListingServices
-        exclude = ['date_created', 'last_updated', 'service']
+        exclude = ['date_created', 'last_updated']
