b33e13f194791b358dd143d7078ffde09b7b825c
willhitman <giftwt9wt@gmail.com>
2024-10-01 16:25:56 +0200

    Updated Dayserializer to alow list creation

--- utils/serializers.py
+++ utils/serializers.py
@@ -1,4 +1,6 @@
-from rest_framework import status
+from django.db import IntegrityError
+from rest_framework import status, serializers
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer, ModelSerializer
 
@@ -35,7 +37,44 @@ class AccessibilitySerializer(ModelSerializer):
         exclude = ['date_created', 'last_updated']
 
 
+class AccessibilityUpdateSerializer(ModelSerializer):
+    class Meta:
+        model = Accessibility
+        exclude = ['date_created', 'last_updated', 'listing']
+
+
+class DaysListSerializer(serializers.ListSerializer):
+    def create(self, validated_data):
+        print(validated_data)
+        validated_days = []
+
+        # Iterate over each item and validate
+        for item in validated_data:
+            if not item or 'day' not in item or not item['day']:
+                raise serializers.ValidationError("Each entry must contain a valid 'day' field.")
+
+            # Validate using the DaysSerializer
+            serializer = DaysSerializer(data=item)
+            if serializer.is_valid():
+                validated_days.append(Days(**serializer.validated_data))
+            else:
+                raise serializers.ValidationError(f"Invalid data: {item}")
+
+        # Bulk create the validated Days objects
+        try:
+            return Days.objects.bulk_create(validated_days)
+        except IntegrityError as e:
+            # Parse the IntegrityError to get the duplicate day
+            if 'Duplicate entry' in str(e):
+                duplicate_value = str(e).split("'")[1]  # Extract the duplicate value from the error message
+                raise ValidationError(f"The day '{duplicate_value}' already exists in the database.")
+            raise ValidationError("A database error occurred.")
+
+
 class DaysSerializer(ModelSerializer):
+    day = serializers.CharField(required=True)
+
     class Meta:
         model = Days
-        exclude = ['date_created', 'last_updated']
+        fields = ['day']
+        list_serializer_class = DaysListSerializer
