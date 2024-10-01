6a77c21d7b3c2f18bc567372ef0ce87311fa3d0a
willhitman <giftwt9wt@gmail.com>
2024-10-01 16:22:42 +0200

    Added  new end points for get update and destroy

--- utils/days/urls.py
+++ utils/days/urls.py
@@ -3,5 +3,6 @@ from rest_framework.urls import path
 from . import views
 
 urlpatterns = [
-    path('days/', views.CreateDayView.as_view(), name='create-day'),
+    path('day/', views.CreateDayView.as_view(), name='create-day'),
+    path('day/<int:pk>/', views.GetUpdateDestroyView.as_view(), name='get-update-destroy-day-view')
 ]
